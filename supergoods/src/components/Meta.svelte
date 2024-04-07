897437f00b587a826028e4b7a649f385ec6f635b
j-wrigley <136844379+j-wrigley@users.noreply.github.com>
2024-04-07 16:41:45 +0100

    Meta Update

--- supergoods/src/components/Meta.svelte
+++ supergoods/src/components/Meta.svelte
@@ -16,12 +16,12 @@
   <meta property="og:url" content="https://supergoods.today/" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
-  <!-- <meta property="og:image" content={imageUrl} /> -->
+  <meta property="og:image" content={imageUrl} />
 
   <!-- Twitter -->
   <meta property="twitter:card" content="summary_large_image" /> <!-- Use "summary" if you prefer a smaller image -->
   <meta property="twitter:url" content="https://supergoods.today/" />
   <meta property="twitter:title" content={title} />
   <meta property="twitter:description" content={description} />
-  <!-- <meta property="twitter:image" content={imageUrl} /> -->
+  <meta property="twitter:image" content={imageUrl} />
 </svelte:head>
